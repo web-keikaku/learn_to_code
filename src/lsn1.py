ff563e6fe92e1bc930c61ad6e53ab1116bb72b5e
和平 <xkazuhira@outlook.jp>
2015-08-24 14:31:35 +0200

    Doing classess

--- src/lsn1.py
+++ src/lsn1.py
@@ -243,4 +243,44 @@ text_in_file = test_file.read()
 
 print(text_in_file)
 
-os.remove("test.txt")
+# os.remove("test.txt")
+
+## PART NINE: END
+########################################################################################################################
+## PART TEN: START
+
+class Animal:
+    __name = None
+    __height = 0
+    __weight = 0
+    __sound = 0
+
+    def __init__(self, name, height, weight, sound):
+        self.__name = name
+        self.__height = height
+        self.__weight = weight
+        self.__sound = sound
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_height(self):
+        return self.__height
+
+    def set_weight(self, weight):
+        self.__weight = weight
+
+    def get_weight(self):
+        return self.__weight
+
+    def set_sound(self, sound):
+        self.__sount = sound
+
+    def get_sound(self):
+        return self.__sound
